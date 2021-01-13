726a1261d7db9d0f3fce041d35aef345ea01ce2c
Barna Mink <barnamink@gmail.com>
2021-01-13 11:14:49 -0800

    .

---
+++ cl/main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include "bstr.h"
+#include "hiredis_helper.h"
+
+void usage(const char *);
+
+#define EDITOR	"/usr/bin/vi"
+
+int
+main(int argc, char **argv)
+{
+	char	*execn;
+	int	err;
+	int	ret;
+	barr_t	*elems;
+	bstr_t	*elem;
+	char	*key;
+	int	c;
+	int	silent;
+
+	err = 0;
+	elems = NULL;
+
+	execn = basename(argv[0]);
+	if(xstrempty(execn)) {
+		fprintf(stderr, "Can't get executable name\n");
+		err = -1;
+		goto end_label;
+	}
+
+	silent = 0;
+	opterr = 0;
+
+	while ((c = getopt (argc, argv, "s")) != -1) {
+		switch (c) {
+		case 's':
+			++silent;
+			break;
+		case '?':
+			fprintf (stderr, "Unknown option `-%c'\n", optopt);
+			err = -1;
+			goto end_label;
+		default:
+			fprintf (stderr, "Error while parsing options");
+			err = -1;
+			goto end_label;
+		}
+	}
+
+	if(argc != optind + 1 || xstrempty(argv[optind])) {
+		usage(execn);
+		err = -1;
+		goto end_label;
+	}
+
+	key = argv[optind];
+
+	ret = hiredis_init();
+	if(ret != 0) {
+		fprintf(stderr, "Could not connect to redis\n");
+		err = -1;
+		goto end_label;
+	}
+
+	elems = barr_init(sizeof(bstr_t));
+	if(elems == NULL) {
+		fprintf(stderr, "Couldn't allocate elems\n");
+		err = -1;
+		goto end_label;
+	}
+
+	ret = hiredis_lrange(key, 0, - 1, elems);
+	if(ret != 0) {
+		fprintf(stderr, "Couldn't lrange: %s\n", strerror(ret));
+		err = -1;
+		goto end_label;
+	}
+
+	if(barr_cnt(elems) == 0) {
+		if(!silent) {
+			fprintf(stderr, "Key doesn't exist\n");
+			err = -1;
+			goto end_label;
+		}
+	} else {
+			for(elem = (bstr_t *) barr_begin(elems);
+		    elem < (bstr_t *) barr_end(elems); ++elem) {
+			printf("%s\n", bget(elem));
+		}
+	}
+
+end_label:
+	
+	if(elems) {
+		for(elem = (bstr_t *) barr_begin(elems);
+		    elem < (bstr_t *) barr_end(elems); ++elem) {
+			buninit_(elem);
+		}
+		barr_uninit(&elems);
+	}
+
+	hiredis_uninit();
+
+	return err;
+
+}
+
+
+void
+usage(const char *execn)
+{
+	printf("usage: %s [-s] <key>\n", execn);
+}
--- el/main.c
+++ el/main.c
@@ -22,6 +22,10 @@ main(int argc, char **argv)
 	bstr_t	*newval;
 	barr_t	*elems;
 	bstr_t	*elem;
+	char	*key;
+	int	c;
+	int	docreate;
+	barr_t	*newelems;
 
 	val = NULL;
 	err = 0;
@@ -29,6 +33,7 @@ main(int argc, char **argv)
 	cmd = NULL;
 	newval = NULL;
 	elems = NULL;
+	newelems = NULL;
 
 	execn = basename(argv[0]);
 	if(xstrempty(execn)) {
@@ -37,11 +42,32 @@ main(int argc, char **argv)
 		goto end_label;
 	}
 
-	if(argc != 2 || xstrempty(argv[1])) {
+	docreate = 0;
+	opterr = 0;
+
+	while ((c = getopt (argc, argv, "c")) != -1) {
+		switch (c) {
+		case 'c':
+			++docreate;
+			break;
+		case '?':
+			fprintf (stderr, "Unknown option `-%c'\n", optopt);
+			err = -1;
+			goto end_label;
+		default:
+			fprintf (stderr, "Error while parsing options");
+			err = -1;
+			goto end_label;
+		}
+	}
+
+	if(argc != optind + 1 || xstrempty(argv[optind])) {
 		usage(execn);
 		err = -1;
 		goto end_label;
 	}
+
+	key = argv[optind];
 
 	ret = hiredis_init();
 	if(ret != 0) {
@@ -64,14 +90,14 @@ main(int argc, char **argv)
 		goto end_label;
 	}
 
-	ret = hiredis_lrange(argv[1], 0, - 1, elems);
+	ret = hiredis_lrange(key, 0, - 1, elems);
 	if(ret != 0) {
 		fprintf(stderr, "Couldn't lrange: %s\n", strerror(ret));
 		err = -1;
 		goto end_label;
 	}
 
-	if(barr_cnt(elems) == 0) {
+	if(barr_cnt(elems) == 0 && !docreate) {
 		fprintf(stderr, "Key doesn't exist\n");
 		err = -1;
 		goto end_label;
@@ -89,12 +115,14 @@ main(int argc, char **argv)
 		goto end_label;
 	}
 	bprintf(filen, "/tmp/%s_%d_%d", execn, getpid(), time(NULL));
-
-	ret = btofile(bget(filen), val);
-	if(ret != 0) {
-		fprintf(stderr, "Couldn't write value to filen\n");
-		err = -1;
-		goto end_label;
+	
+	if(!bstrempty(val)) { /* OK to be empty if we are creating */
+		ret = btofile(bget(filen), val);
+		if(ret != 0) {
+			fprintf(stderr, "Couldn't write value to filen\n");
+			err = -1;
+			goto end_label;
+		}
 	}
 
 	cmd = binit();
@@ -120,19 +148,39 @@ main(int argc, char **argv)
 	}
 
 	ret = bfromfile(newval, bget(filen));
-	if(ret != 0) {
+	if(ret != 0 && !docreate) {
+		/* OK for file to not exist if we are creating. Just means
+		 * "unchanged". */
 		fprintf(stderr, "Couldn't load file\n");
 		err = -1;
 		goto end_label;
 	}
 
 	if(!bstrcmp(val, bget(newval))) {
-		printf("Value not changed.\n");
+		if(docreate && bstrempty(newval))
+			printf("List not created.\n");
+		else
+			printf("List not changed.\n");
 		goto end_label;
 	}
 
+	ret = bstrsplit(newval, "\n", 0, &newelems);
+	if(ret != 0 || newelems == NULL) {
+		fprintf(stderr, "Couldn't split new value\n");
+		err = -1;
+		goto end_label;
+	}
+	if(barr_cnt(newelems) == 0) {
+		fprintf(stderr, "New list can't be empty, use -d to delete"
+		    " a list\n");
+		err = -1;
+		goto end_label;
+	}
+	
+	printf("%d values.\n", barr_cnt(newelems));
+
 #if 0
-	ret = hiredis_set(argv[1], newval);
+	ret = hiredis_set(key, newval);
 	if(ret != 0) {
 		fprintf(stderr, "Could not update value in redis.\n");
 		err = -1;
@@ -160,6 +208,13 @@ end_label:
 		}
 		barr_uninit(&elems);
 	}
+	if(newelems) {
+		for(elem = (bstr_t *) barr_begin(newelems);
+		    elem < (bstr_t *) barr_end(newelems); ++elem) {
+			buninit_(elem);
+		}
+		barr_uninit(&newelems);
+	}
 
 	hiredis_uninit();
 
@@ -171,5 +226,5 @@ end_label:
 void
 usage(const char *execn)
 {
-	printf("usage: %s <key>\n", execn);
+	printf("usage: %s [-c] <key>\n", execn);
 }
